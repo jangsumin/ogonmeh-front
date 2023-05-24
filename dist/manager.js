4b01e7daf57964488213adb23bd3bdcf5423fe6e
jangsumin <jsm0726@naver.com>
2023-05-24 15:56:44 +0900

    feat: form 요소 내부의 날짜 정보를 전달

--- dist/manager.js
+++ dist/manager.js
@@ -1,5 +1,10 @@
 "use strict";
 const form = document.querySelector(".menu-input-form");
+// 메인 페이지의 dateText 변수와 충돌하므로 namespace 사용
+var NamespaceManager;
+(function (NamespaceManager) {
+    NamespaceManager.dateText = document.querySelector(".date-text");
+})(NamespaceManager || (NamespaceManager = {}));
 const updateButton = document.querySelector(".update-button");
 let formData;
 const data = {
@@ -11,18 +16,19 @@ const KOREAN_FOOD_MENU = "korean-food-menu";
 const HOT_MENU = "hot-menu";
 const SALAD_MENU = "salad-menu";
 updateButton === null || updateButton === void 0 ? void 0 : updateButton.addEventListener("click", () => {
-    var _a, _b, _c;
+    var _a, _b, _c, _d;
     if (form) {
+        data.date = String((_a = NamespaceManager.dateText) === null || _a === void 0 ? void 0 : _a.textContent);
         formData = new FormData(form);
         for (let [key, value] of formData.entries()) {
             if (key.slice(0, -1) === KOREAN_FOOD_MENU) {
-                (_a = data["koreanFoodCorner"]) === null || _a === void 0 ? void 0 : _a.push(String(value));
+                (_b = data.koreanFoodCorner) === null || _b === void 0 ? void 0 : _b.push(String(value));
             }
             if (key.slice(0, -1) === HOT_MENU) {
-                (_b = data["hotCorner"]) === null || _b === void 0 ? void 0 : _b.push(String(value));
+                (_c = data.hotCorner) === null || _c === void 0 ? void 0 : _c.push(String(value));
             }
             if (key.slice(0, -1) === SALAD_MENU) {
-                (_c = data["saladCorner"]) === null || _c === void 0 ? void 0 : _c.push(String(value));
+                (_d = data.saladCorner) === null || _d === void 0 ? void 0 : _d.push(String(value));
             }
         }
     }
--- src/manager.ts
+++ src/manager.ts
@@ -1,11 +1,16 @@
 interface Menu {
   date?: String;
-  koreanFoodCorner?: String[];
-  hotCorner?: String[];
-  saladCorner?: String[];
+  koreanFoodCorner: String[];
+  hotCorner: String[];
+  saladCorner: String[];
 }
 
 const form: HTMLFormElement | null = document.querySelector(".menu-input-form");
+// 메인 페이지의 dateText 변수와 충돌하므로 namespace 사용
+namespace NamespaceManager {
+  export const dateText: HTMLInputElement | null =
+    document.querySelector(".date-text");
+}
 const updateButton: HTMLInputElement | null =
   document.querySelector(".update-button");
 let formData: FormData | null;
@@ -21,16 +26,17 @@ const SALAD_MENU: String = "salad-menu";
 
 updateButton?.addEventListener("click", () => {
   if (form) {
+    data.date = String(NamespaceManager.dateText?.textContent);
     formData = new FormData(form);
     for (let [key, value] of formData.entries()) {
       if (key.slice(0, -1) === KOREAN_FOOD_MENU) {
-        data["koreanFoodCorner"]?.push(String(value));
+        data.koreanFoodCorner?.push(String(value));
       }
       if (key.slice(0, -1) === HOT_MENU) {
-        data["hotCorner"]?.push(String(value));
+        data.hotCorner?.push(String(value));
       }
       if (key.slice(0, -1) === SALAD_MENU) {
-        data["saladCorner"]?.push(String(value));
+        data.saladCorner?.push(String(value));
       }
     }
   }
